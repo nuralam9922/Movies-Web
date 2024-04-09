8f9b0a07f73f852d880210a90c1f29889f143d17
your name here <abc@test.com>
2024-04-09 10:54:17 +0530

    fix loading problem in viewAll page

--- src/pages/ViewAll.jsx
+++ src/pages/ViewAll.jsx
@@ -4,6 +4,29 @@ import { useParams } from 'react-router-dom';
 import { FetchApi } from '../api/FetchApi';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+export  function LoadingCart() {
+	return (
+		<div className="px-6">
+			<div className="animate-pulse h-screen grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 overflow-hidden">
+				{[...Array(10)].map((_, i) => (
+					<div key={i} className=" text-white duration-200 snap-start">
+						{/* Skeleton image */}
+						<div className="rounded-md bg-gray-300 hover:scale-105 duration-300 cursor-pointer w-full h-72 aspect-w-2 aspect-h-3">
+							{/* Placeholder for image */}
+						</div>
+						{/* Skeleton title */}
+						<div className="h-6 bg-gray-300 rounded-md animate-pulse mt-2 w-full" />
+						{/* Skeleton description */}
+						<div className="h-4 bg-gray-300 rounded-md animate-pulse mt-1 w-full" />
+						{/* Skeleton rating */}
+						<div className="h-4 bg-gray-300 rounded-md animate-pulse mt-1 w-full" />
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
 function ViewAll() {
 	useEffect(() => {
 		window.scrollTo(0, 0);
@@ -52,7 +75,7 @@ function ViewAll() {
 				dataLength={movies.length}
 				next={fetchMoreData}
 				hasMore={hasMore}
-				loader={<div className="px-6">Loading...</div>}
+				loader={<LoadingCart/>}
 				endMessage={<p className="text-center text-4xl font-bold">No more movies to load</p>}
 			>
 				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 overflow-hidden">
